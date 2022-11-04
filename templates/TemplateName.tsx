9577423f9203b5299a7457842800803d14fab5b9
The-Professor99 <festusihechi99@gmail.com>
2022-11-04 17:49:03 +0100

    Add templates to be used for Generate-React-CLI files generation

---
+++ templates/TemplateName.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TemplateName.scss";
+
+function TemplateName() {
+  return (
+    <div className="TemplateName" data-testid="TemplateName">
+      TemplateName Component
+    </div>
+  );
+}
+
+export { TemplateName };
